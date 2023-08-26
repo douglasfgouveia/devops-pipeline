07e941e9a31548fdb87cc2b63817dbe189e9f878
Douglas F. Gouveia <88691724+douglasfgouveia@users.noreply.github.com>
2023-08-26 09:30:36 -0300

    Update test_addition.py

--- test_addition.py
+++ test_addition.py
@@ -2,17 +2,17 @@ import addition
 import pytest
 
 def test_add():
-    assert addtion.add(1, 2) == 3
-    assert addtion.add(1, 1) == 2
-    assert addtion.add(1, 0) == 1
-    assert addtion.add(1, -1) == 0
-    assert addtion.add(1, -2) == -1
-    assert addtion.add(1, -3) == -2 
+    assert addition.add(1, 2) == 3
+    assert addition.add(1, 1) == 2
+    assert addition.add(1, 0) == 1
+    assert addition.add(1, -1) == 0
+    assert addition.add(1, -2) == -1
+    assert addition.add(1, -3) == -2 
 
 def test_sub():
-    assert addtion.sub(1, 2) == -1
-    assert addtion.sub(1, 1) == 0
-    assert addtion.sub(1, 0) == 1
-    assert addtion.sub(1, -1) == 2
-    assert addtion.sub(1, -2) == 3
-    assert addtion.sub(1, -3) == 4
+    assert addition.sub(1, 2) == -1
+    assert addition.sub(1, 1) == 0
+    assert addition.sub(1, 0) == 1
+    assert addition.sub(1, -1) == 2
+    assert addition.sub(1, -2) == 3
+    assert addition.sub(1, -3) == 4
